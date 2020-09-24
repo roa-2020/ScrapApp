1929428727e863db38642e3cf005f8cf68be9a89
Lewis Rosenberg Smith <lrosenbergsmith@gmail.com>
2020-09-24 15:23:19 +1200

    got map onscreen

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
+
+import Map from './Map'
 import Login from './Login'
 import Register from './Register'
 import Nav from './Nav'
@@ -16,28 +18,29 @@ export class App extends React.Component {
   render() {
     const {auth} = this.props
     return (
-      <Router>
-        <div className="container has-text-centered">
+      <Map/>
+      // <Router>
+      //   <div className="container has-text-centered">
 
-          <div className="hero is-small is-primary">
-            <div className="hero-body has-text-centered">
-              <Link to='/' className="">
-                <h1 className="title is-1">Greetings!</h1>
-              </Link>
-              <Route path="/" component={Nav} />
-            </div>
-          </div>
+      //     <div className="hero is-small is-primary">
+      //       <div className="hero-body has-text-centered">
+      //         <Link to='/' className="">
+      //           <h1 className="title is-1">Greetings!</h1>
+      //         </Link>
+      //         <Route path="/" component={Nav} />
+      //       </div>
+      //     </div>
 
-          <div className=''>
-            {!auth.isAuthenticated &&
-              <Route exact path="/" component={Login} />
-            }
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-          </div>
+      //     <div className=''>
+      //       {!auth.isAuthenticated &&
+      //         <Route exact path="/" component={Login} />
+      //       }
+      //       <Route path="/login" component={Login} />
+      //       <Route path="/register" component={Register} />
+      //     </div>
 
-        </div>
-      </Router>
+      //   </div>
+      // </Router>
     )
   }
 }
