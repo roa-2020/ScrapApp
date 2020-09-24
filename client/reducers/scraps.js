693a10011fabee698f778ff4ad4c736718c2bfe1
steven <steven.norhed@gmail.com>
2020-09-25 10:15:23 +1200

    fleshing out actions and reducers, pushing as Phil needs to use them to continue his work

--- client/reducers/scraps.js
+++ client/reducers/scraps.js
@@ -1,4 +1,4 @@
-import { RECIEVE_SCRAP } from '../actions/scraps'
+import { ADD_SCRAP, RECIEVE_SCRAP } from '../actions/scraps'
 import { DELETE_SCRAP } from '../actions/scraps'
 
 const initialState = {
@@ -10,8 +10,13 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case RECIEVE_SCRAP:
             return action.scrapDetails
-        // case DELETE_SCRAP:
-        // add return case for delete
+
+        case ADD_SCRAP:
+            return [...state, action.scrapDetails]
+        
+        case DELETE_SCRAP:
+            return state.filter((scrapDetails) => scrapDetails !== action.scrapDetails)
+        
         default:
             return state
     }
