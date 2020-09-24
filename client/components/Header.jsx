29cf7c66d850b6fb0caa75ba1f0d07adab181af4
cameron <cameronshaw95@gmail.com>
2020-09-24 15:15:19 +1200

    add header component

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 
 class Header extends React.Component {
     render() {
         return (
             <>
-                <FontAwesomeIcon icon={faUserCircle} />
+                <header className="columns is-centered">
+                    <FontAwesomeIcon icon={faUserCircle} size="4x" className="level-item" />
+                    <h1 className="title level-item">Scrap</h1>
+                    <FontAwesomeIcon icon={faPlusCircle} size="4x" className="level-item" />
+                </header>
             </>
         )
     }
