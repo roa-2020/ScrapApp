c3cee01ace43b42882d835a3f5c58a0882fad508
philc <32604473+Phil160@users.noreply.github.com>
2020-09-25 15:02:30 +1200

    preview shows first 3 scraps in array

--- client/components/ScrapPreview.jsx
+++ client/components/ScrapPreview.jsx
@@ -3,23 +3,13 @@ import { connect } from 'react-redux'
 
 class ScrapPreview extends React.Component {
 
-  // componentDidMount() {
-  //   // scrapsArr.sort(() => 0.5 - Math.random()).slice(0, 2)
-  //   let scrapsArr = []
-  //   if (this.props.scraps.length > 0) {
-  //     scrapsArr = [...this.props.scraps]
-  //     scrapsArr.slice(0, 2)
-  //     console.log(1, scrapsArr)
-  //   }
-  // }
-
   render() {
     return (
       <>
         <h1>Preview of scraps</h1>
         <ul>
           {
-            this.props.scraps.map(scrap => {
+            this.props.scraps.slice(0, 3).map(scrap => {
               return (
                 <>
                   <li key={scrap.id} >{scrap.category}: {scrap.scrap_name}</li>
