e6553fe2b09ca2d6fff87542c8410e2ccca9c1c7
philc <32604473+Phil160@users.noreply.github.com>
2020-09-25 14:28:56 +1200

    scrappreview random array

--- client/components/ScrapPreview.jsx
+++ client/components/ScrapPreview.jsx
@@ -4,16 +4,26 @@ import { connect } from 'react-redux'
 import { getAllScraps } from '../actions/scraps'
 import { apiGetScraps } from '../apis/scrap'
 
+let scrapsArray = this.props.scraps
+
 class ScrapPreview extends React.Component {
   componentDidMount() {
     apiGetScraps().then(scraps => this.props.dispatch(getAllScraps(scraps)))
-    console.log(this.props)
   }
 
   render() {
+    const shuffled = scrapsArray.sort(() => Math.random() - Math.random()).slice(0, n)
+    console.log(scrapsArray)
+    console.log(shuffled)
     return (
       <>
-
+        <ul>
+          {/* {
+            this.props.scraps.map((scrap, idx) => {
+              return <li>{scrap.category}: {scrap.scrap_name}</li>
+            })
+          } */}
+        </ul>
       </>
     )
   }
