4d965f50d46197d5f0a83a72b93bf0d9ebbbd1ce
seamus-ahkit <seamusae@gmail.com>
2020-09-25 16:45:02 +1200

    Markers use global state

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -31,6 +31,8 @@ class Map extends React.Component{
 
 const selectedScrap = this.state.selectedScrap
 
+console.log(this.props.scraps)
+
   return (
     <div id="map">
     <ReactMapGL 
@@ -46,12 +48,12 @@ const selectedScrap = this.state.selectedScrap
         //on page load centre on user
         auto={true}
         />
-        {scrapData.map((scrap) => (
+        {this.props.scraps.map((scrap) => (
 
             <Marker 
               key={scrap.id}
-              latitude={scrap.latitude[0]}
-              longitude={scrap.longitude[0]}
+              latitude={scrap.latitude}
+              longitude={scrap.longitude}
             >
               <button className="marker-btn" 
                 onClick={e => {
@@ -67,16 +69,15 @@ const selectedScrap = this.state.selectedScrap
 
         {selectedScrap && (
           <Popup 
-            latitude={selectedScrap.latitude[0]} 
-            longitude={selectedScrap.longitude[0]}
+            latitude={selectedScrap.latitude} 
+            longitude={selectedScrap.longitude}
             onClose={() => {
               this.changeScrap(null)
             }}
           >
 
             <div>
-              <p className="title is-5">{selectedScrap.scrap_name}</p>
-              <p className="title is-6">Category: {selectedScrap.category}</p>
+              <p className="title is-6">{selectedScrap.scrap_name} - {selectedScrap.category}</p>
               <p className="subtitle is-6">{selectedScrap.description}</p>
             </div>
 
@@ -88,10 +89,8 @@ const selectedScrap = this.state.selectedScrap
   )
 }
 }
-const mapStateToProps = ({ auth }) => {
-  return {
-    auth
-  }
+function mapStateToProps(globalState) {
+  return { scraps: globalState.scraps }
 }
 
 export default connect(mapStateToProps)(Map)
