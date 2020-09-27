2b5aefe5cbff8d72ad896b66922ddedc764c442e
philc <32604473+Phil160@users.noreply.github.com>
2020-09-28 10:37:28 +1300

    navbar format and working, remove hamburger as well

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -67,8 +67,8 @@ export class App extends React.Component {
             </main>
             <Route exact path="/user" component={Nav} />
             <Route exact path="/" component={Footer} />
-            <Route exact path="/scraps/add" component={AddScrapForm} />
             <Route exact path="/scraps/add" component={Nav} />
+            <Route exact path="/scraps/add" component={AddScrapForm} />
           </>
         }
       </Router>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -5,45 +5,16 @@ import { logoutUser } from '../actions/auth'
 
 class Nav extends React.Component {
   state = {
-    showBurger: false
-  }
-
-  toggleBurger = () => {
-    this.setState({
-      showBurger: !this.state.showBurger
-    })
   }
 
   render() {
     const { auth, logout } = this.props
-    const { showBurger } = this.state
     return <nav className="navbar">
-      <div className="container">
-        <div className="navbar-brand">
-          <span onClick={this.toggleBurger} className={`navbar-burger burger ${showBurger ? 'is-active' : ''}`} data-target="navbarMenuHeroA">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-        </div>
-        <div id="navbarMenuHeroA" className={`navbar-menu ${showBurger ? "is-active" : ''}`}>
-          <div className="navbar-end">
-            {auth.isAuthenticated
-              ? (
-                <>
-                  <Link to='/' className="navbar-item is-large">Map</Link>
-                  <Link to='/' className="navbar-item is-large" onClick={() => logout()}>Logout</Link>
-                </>
-              )
-              : (
-                <>
-                  <Link onClick={this.toggleBurger} className="navbar-item is-large" to='/login'>Login</Link>
-                  <Link onClick={this.toggleBurger} className="navbar-item" to='/register'>Register</Link>
-                </>
-              )
-            }
-          </div>
-        </div>
+      <div className="navbar-brand">
+        <>
+          <Link to='/' className="navbar-item column">Map</Link>
+          <Link to='/' className="navbar-item column" onClick={() => logout()}>Logout</Link>
+        </>
       </div>
     </nav >
   }
