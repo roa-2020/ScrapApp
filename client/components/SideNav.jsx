32ab7cfcfe27d1b3b20b73c60c71d2213e8f0abf
Ross Jourdain <>
2020-09-29 10:46:00 +1300

    WIP

---
+++ client/components/SideNav.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default class Sidebar extends React.Component {
+  state = {
+    menuOpen: false,
+  };
+
+  toggleMenu = () => {
+    console.log("open");
+    this.setState({ menuOpen: !this.state.menuOpen });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.toggleMenu} className="toggle-menu">
+          click me
+        </button>
+
+        <div
+          className={[
+            "side-bar",
+            this.state.menuOpen ? "side-bar-open" : "side-bar-closed",
+          ].join(" ")}
+        >
+          <Profile />
+          
+        </div>
+      </React.Fragment>
+    );
+  }
+}
