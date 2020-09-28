72fe2c50e56d690a920543433648c2358fb0a9f6
steven <steven.norhed@gmail.com>
2020-09-28 16:48:05 +1300

    profile side-menu transition working, still need to change some positioning to work better though

--- client/components/SideNav.jsx
+++ client/components/SideNav.jsx
@@ -1,46 +1,32 @@
 import React from "react";
 
 export default class Sidebar extends React.Component {
-  
-    toggleMenu = () => {
-        console.log("open")
-    this.setState({ menuOpen: !this.state.menuOpen });
-  };
-
   state = {
     menuOpen: false,
   };
 
-  render() {
+  toggleMenu = () => {
+    console.log("open");
+    this.setState({ menuOpen: !this.state.menuOpen });
+  };
 
+  render() {
     return (
       <React.Fragment>
-           <button
-            onClick={this.toggleMenu}
-            className="toggle-menu"
-            style={
-              {
-                zIndex: "234567891",
-                position:"fixed",
-                marginTop: "20px"
-                // transform: `translate(${width}px, 20vh)`,
-              }
-            }
-          > click me </button>
-        { this.state.menuOpen && <div
-          className="side-bar"
-          style={{
-            //   transform: `translatex(${xPosition}px)`,
-            marginTop: "35px",
-            width: "400px",
-            minHeight: "1080px",
-            backgroundColor: "grey",
-            position:"fixed",
-            zIndex: "234567890"
-          }}
+        <button onClick={this.toggleMenu} className="toggle-menu">
+          click me
+        </button>
+
+        <div
+          className={[
+            "side-bar",
+            this.state.menuOpen ? "side-bar-open" : "side-bar-closed",
+          ].join(" ")}
         >
-          <div className="content">{this.props.children}</div>
-        </div>}
+          <form>
+            <input placeholder="tings"></input>
+          </form>
+        </div>
       </React.Fragment>
     );
   }
