a315c74065ef009a696450c055d3640ff9dfd86c
seamus-ahkit <seamusae@gmail.com>
2020-09-29 16:44:40 +1300

    Seed file to fix POST issue on Heroku

---
+++ seeds/z_reset_seeds.js
@@ -0,0 +1,28 @@
+exports.seed = (knex) => {
+    // Reset the sequences in postgres
+    if (knex.client.config.client == "postgresql") {
+      return knex("pg_class")
+        .where("relkind", "S")
+        .not.where("relname", "LIKE", "%migration%")
+        .select("relname")
+        .debug()
+        .then((sequences) => {
+          return Promise.all(
+            sequences.map((sequenceObj) => {
+              console.log(sequenceObj)
+              const sequence = sequenceObj.relname
+              const table = sequence.replace("_id_seq", "")
+              console.log(sequence, table)
+              console.log(
+                `select setval('${sequence}', (select max(id) from ${table}) + 1)`
+              )
+              return knex.raw(
+                `select setval('${sequence}', (select max(id) from ${table}) + 1)`
+              )
+            })
+          ).then((results) => {
+            console.log(results)
+          })
+        })
+    }
+  }
